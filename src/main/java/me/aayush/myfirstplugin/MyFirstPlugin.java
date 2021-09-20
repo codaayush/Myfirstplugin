46430af302ffb852f3a3da3077ee1516e43e8735
PreranaCodes <inspiresprerana@gmail.com>
2021-09-20 08:26:56 +0530

    Initial commit

---
+++ src/main/java/me/aayush/myfirstplugin/MyFirstPlugin.java
@@ -0,0 +1,26 @@
+package me.aayush.myfirstplugin;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MyFirstPlugin extends JavaPlugin implements Listener {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        System.out.println("the plugin is started");
+
+        getServer().getPluginManager().registerEvents(this, this);
+
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event){
+
+        System.out.println("the player has join kindly see him");
+
+    }
+
+}
